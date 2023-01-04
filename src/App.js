cec2a76a9c693158ca8e5e9fc1e9f01f6d17c732
claudiuj <c.jude@reply.com>
2023-01-04 17:29:49 +0200

    initial create-react-app setup + bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Login from './pages/login/login';
+//import './index.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Login />
+        // <div class="container">
+        //     <div class="row">
+        //         <div class="col-md-6 offset-md-4 login-form__wrapper">
+        //             <form>
+
+        //             </form>
+        //         </div>
+        //     </div>
+        // </div>
+    );
 }
 
 export default App;
